11f5b285ba4258f7a254445da6e1bb0e94ca896d
voxace <dave@voxace.com>
2018-07-21 17:05:47 +1000

    Beginning keyboard input

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        // Thrust
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space");
+        }
+
+        // Rotate
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right");
+        }
+    }
+}
